0bf5666577d508e8656d9b1da38c922367441f79
John Moen <entity.john@gmail.com>
2019-02-07 09:00:47 -0600

    correct API key import to match .env file

--- src/AutoSuggestPlaces.js
+++ src/AutoSuggestPlaces.js
@@ -1,15 +1,17 @@
-const googlePlaces = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API}&libraries=places`;
+const googlePlaces = `https://maps.googleapis.com/maps/api/js?key=${
+  process.env.REACT_APP_GOOGLE_MAPS_API
+}&libraries=places`;
 console.log(window.google);
 
-getGooglePlacesSuggestions = (input) => {
+getGooglePlacesSuggestions = input => {
   axios
-    .get('https://maps.googleapis.com/maps/api/place/autocomplete/json', {
+    .get("https://maps.googleapis.com/maps/api/place/autocomplete/json", {
       params: {
-        key: `${process.env.REACT_APP_GOOGLE_MAPS_API}`,
-        input: `${input}`,
-      },
+        key: `${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`,
+        input: `${input}`
+      }
     })
-    .then((response) => {
+    .then(response => {
       console.log(response.data);
-    })
-}
+    });
+};
