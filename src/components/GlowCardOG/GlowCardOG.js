0c2a1aea80fe2ce4cbfbefa04bf002df0877f204
John Moen <entity.john@gmail.com>
2019-02-13 06:22:08 -0600

    add originally formatted Glowing Card from codepen
    https://codepen.io/dissolutio/pen/YBjxOM

---
+++ src/components/GlowCardOG/GlowCardOG.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import "./glowcardOG.scss";
+
+export default function GlowCardOG() {
+  return (
+    <div id="glowcard-container">
+      <div class="myview">
+        <ul>
+          {" "}
+          <li>UI/UX</li> <li>UI/UX</li> <li>UI/UX</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
