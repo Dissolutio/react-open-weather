2bfe9e3a0c750fbcfc45a438e37aeee158750551
John Moen <entity.john@gmail.com>
2019-02-13 10:24:11 -0600

    use GlowCard in App, all stable and working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import PlaceForm from "./Weather/PlaceForm";
 import GlowCard from "./components/GlowCard/GlowCard";
+import GlowCardOG from "./components/GlowCardOG/GlowCardOG";
 import BigGloButton from "./components/BigGloButton/BigGloButton";
 
 const AppWrapper = styled.div`
@@ -10,15 +11,12 @@ const AppWrapper = styled.div`
   justify-content: center;
   align-items: center;
   min-height: 100vh;
-  width: 100vw;
+  width: 100%;
   margin: 0;
   font-family: sans-serif;
   font-size: 16px;
   background: #abbaab;
   background: linear-gradient(135deg, #ffffff, #abbaab);
-`;
-const BlackWrapper = styled.div`
-  background-color: #000;
 `;
 
 class App extends Component {
@@ -28,12 +26,9 @@ class App extends Component {
         <AppWrapper>
           <PlaceForm />
         </AppWrapper>
-        <BlackWrapper>
-          <GlowCard />
-        </BlackWrapper>
-        <BlackWrapper>
-          <BigGloButton />
-        </BlackWrapper>
+        <GlowCardOG />
+        <GlowCard />
+        <BigGloButton />
       </>
     );
   }
