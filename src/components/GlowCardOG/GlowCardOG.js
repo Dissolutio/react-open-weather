db569ed0e6432054f47678cc77e580afc8809971
John Moen <entity.john@gmail.com>
2019-02-13 10:18:46 -0600

    GlowCardOG really needs a wrapper to fit into page cleanly

--- src/components/GlowCardOG/GlowCardOG.js
+++ src/components/GlowCardOG/GlowCardOG.js
@@ -4,13 +4,24 @@ import "./glowcardOG.scss";
 
 export default function GlowCardOG() {
   return (
-    <div id="glowcard-container">
-      <div class="myview">
-        <ul>
-          {" "}
-          <li>UI/UX</li> <li>UI/UX</li> <li>UI/UX</li>
-        </ul>
+    <BlackWrapper>
+      <div id="glowcard-container">
+        <div class="myview">
+          <ul>
+            {" "}
+            <li>UI/UX</li> <li>UI/UX</li> <li>UI/UX</li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </BlackWrapper>
   );
 }
+const BlackWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
+  background-color: #000;
+  position: relative;
+  z-index: 1;
+`;
