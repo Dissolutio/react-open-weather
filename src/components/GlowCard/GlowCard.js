251b5c620f5dd93969c4d4b658b89aee4f79014d
John Moen <entity.john@gmail.com>
2019-02-13 08:50:35 -0600

    refactor file, update variable names

--- src/components/GlowCard/GlowCard.js
+++ src/components/GlowCard/GlowCard.js
@@ -2,47 +2,18 @@ import React from "react";
 import styled, { keyframes } from "styled-components";
 import GlowList from "./GlowList";
 
-export default function GlowCard() {
-  const GlowCard = styled.div`
-    border-radius: 8px;
-    position: relative;
-    margin: 70px auto 0;
-    width: 400px;
-    height: 400px;
-    background-image: linear-gradient(-45deg, #000, #111);
-    text-align: center;
-    :before,
-    :after {
-      content: "";
-      border-radius: 10px;
-      position: absolute;
-      left: -2px;
-      top: -2px;
-      width: calc(100% + 4px);
-      height: calc(100% + 4px);
-      background-image: linear-gradient(
-        45deg,
-        #ff0000,
-        #ffff00,
-        #00ff00,
-        #0000ff,
-        #00ffff
-      );
-      background-size: 400%;
-      z-index: -1;
-      animation: ${move} 10s alternate infinite;
-    }
-    :after {
-      filter: blur(10px);
-    }
-  `;
+export default function GlowCardBlock() {
   return (
-    <GlowCard>
-      <GlowList />
-    </GlowCard>
+    <BlackWrapper>
+      <GlowCardContainer>
+        <GlowCard>
+          <GlowList />
+        </GlowCard>
+      </GlowCardContainer>
+    </BlackWrapper>
   );
 }
-const move = keyframes`
+const backgroundShift = keyframes`
     0% {
         background-position: 0 0;
     }
@@ -52,4 +23,55 @@ const move = keyframes`
     100% {
         background-position: 0 0;
     }
+`;
+const BlackWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
+  background-color: #000;
+  position: relative;
+  z-index: 1;
+`;
+const GlowCardContainer = styled.div`
+  padding: 0px;
+  height: 0px;
+  ul,
+  li {
+    padding: 0px;
+    height: 0px;
+  }
+`;
+const GlowCard = styled.div`
+  border-radius: 8px;
+  position: relative;
+  margin: 70px auto 0;
+  width: 50%;
+  height: 400px;
+  background-image: linear-gradient(-45deg, #000, #111);
+  text-align: center;
+  :before,
+  :after {
+    content: "";
+    border-radius: 10px;
+    position: absolute;
+    left: -2px;
+    top: -2px;
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    z-index: -1;
+    background-image: linear-gradient(
+      45deg,
+      #ff0000,
+      #ffff00,
+      #00ff00,
+      #0000ff,
+      #00ffff
+    );
+    background-size: 400%;
+    animation: ${backgroundShift} 10s alternate infinite;
+  }
+  :after {
+    filter: blur(10px);
+  }
 `;
